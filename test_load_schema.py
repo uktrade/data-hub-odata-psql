8c686c1af5f53139208718e36744c41f10f70792
bmcorser <bmcorser@gmail.com>
2016-09-26 13:48:18 +0000

    Poll for connection in OData -> PSQL test

--- test_load_schema.py
+++ test_load_schema.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from lxml import etree
 import psycopg2
@@ -10,11 +11,17 @@ from separate_constraints import main as separate_constraints
 TEST_SERVICE_URL = 'http://services.odata.org/V2/(S(readwrite))/OData/OData.svc/'
 ATTRIB_BASE = '{http://www.w3.org/XML/1998/namespace}base'
 
+def get_connection():
+    while True:
+        try:
+            return psycopg2.connect(os.environ['DATABASE_URL'])
+        except psycopg2.OperationalError as exc:
+            time.sleep(1)
+
 
 def test_load_schema():
     'Test XML metadata can be split into valid CREATE and ALTER statements'
-    connection = psycopg2.connect(os.environ['DATABASE_URL'])
-    cursor = connection.cursor()
+    cursor = get_connection().cursor()
     base_url = etree.fromstring(requests.get(TEST_SERVICE_URL).content).attrib[ATTRIB_BASE]
     with open('metadata.xml', 'wb') as metadata_fh:
         metadata_fh.write(requests.get(base_url + '$metadata').content)
